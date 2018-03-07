65190557358efd7f355114061d16f5ea99ec6703
Nick Kasten <kastencode@gmail.com>
2018-03-07 13:21:01 -0600

    cleaned up comments

--- GriffinChess/src/com/griffin/chess/players/Player.java
+++ GriffinChess/src/com/griffin/chess/players/Player.java
@@ -1,8 +1,5 @@
 package com.griffin.chess.players;
 
-// we should make Player an interface
-// then have a human and a robot class that implement the interface
-
 import com.griffin.chess.pieces.Piece;
 
 import java.util.ArrayList;
